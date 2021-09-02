5b778c0e1d523c101d9874f8087688939d3fc64a
AldanaNonnenmacher <aldananonnenmacher@gmail.com>
2021-09-02 11:23:52 -0300

    commit final

--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,12 +1,21 @@
 package launcher;
 
+import entities.Student;
+import gui.SimplePresentationScreen;
+
 public class Launcher {
 	public static void main(String [] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+       
+		Student alumno = new Student(126995, "Nonnenmacher Galparoli", "Maria Aldana", "aldananonnenmacher@gmail.com", "https://github.com/AldanaNonnenmacher/tdp-proyecto-1", "/images/imagen.jpeg");
+
+		
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-            	
+            	SimplePresentationScreen presentacion = new SimplePresentationScreen(alumno);
+            	presentacion.setVisible(true);
+
             }
         });
     }
